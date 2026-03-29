9c1163a2f0f6938fb4cd6780a66c8560492f6463
fildzahalfitri <fildzahalfitri@student.ub.ac.id>
2026-03-29 13:45:31 +0700

    Update Motor.java

--- StudyCase1/Motor.java
+++ StudyCase1/Motor.java
@@ -7,13 +7,11 @@ public class Motor extends Kendaraan {
         this.jumlahHelm = jumlahHelm;
     }
 
-    // POLIMORFISME: Perhitungan bayar khusus Motor (Ada sistem Diskon!)
     @Override
     public double hitungTotalBayar(int jam) {
         int jumlahPaket = hitungJumlahPaket(jam);
         double totalHarga = jumlahPaket * hargaPer6Jam;
         
-        // Logika Diskon: Kalau sewa minimal 4 paket (24 jam), dapat diskon 10%
         if (jumlahPaket >= 4) {
             double diskon = totalHarga * 0.10;
             totalHarga = totalHarga - diskon; 
@@ -22,14 +20,11 @@ public class Motor extends Kendaraan {
         return totalHarga;
     }
 
-    // POLIMORFISME: Menampilkan info khusus Motor
-    @Override
     public void tampilkanInfo() {
         System.out.println("======= DETAIL SEPEDA MOTOR =======");
-        tampilkanInfoDasar(); // Panggil fungsi dari induk
+        tampilkanInfoDasar(); 
         System.out.printf("%-15s : %d buah\n", "Jumlah Helm", jumlahHelm);
         
-        // IDE CURIAN DARI TEMAN 1: Tambahin info fasilitas biar kelihatan realistis!
         System.out.printf("%-15s : %s\n", "Fasilitas Free", "Jas Hujan (2), STNK, Bensin Full");
         
         System.out.printf("%-15s : %s\n", "Promo Khusus", "Diskon 10% untuk sewa >= 24 Jam!");
